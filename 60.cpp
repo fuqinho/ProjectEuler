32dff3d82ad9511a33fd05e7b4d11017b6760003
fuqinho <kitakita7@gmail.com>
2011-12-09 19:37:20 +0900

    solved 60

--- 60.cpp
+++ 60.cpp
@@ -4,7 +4,7 @@
 #include <cmath>
 using namespace std;
 
-static const long long MAX_PRIME = 100000;
+static const long long MAX_PRIME = 10000;
 int upperBound = 100000;
 
 long long concat(long long a, long long b) {
@@ -17,48 +17,30 @@ long long concat(long long a, long long b) {
     return res + b;
 }
 
-bool isPrime(long long n, const vector<long long>& primes) {
-    if (n <= MAX_PRIME) {
-        if (find(primes.begin(), primes.end(), n) != primes.end())
-            return true;
-        else
-            return false;
-    } else {
-        for (long long i = 2; i <= sqrt(n); i++) {
-            if (n % i == 0) return false;
-        }
-        return true;
-    }
-}
-
-bool isOK(const vector<long long>& primes, vector<long long>& v, long long n) {
-    for (long long i = 0; i < v.size(); i++) {
-        if (isPrime(concat(primes[v[i]], n), primes) == false) return false;
-        if (isPrime(concat(n, primes[v[i]]), primes) == false) return false;
+bool isPrime(long long n) {
+    for (long long i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) return false;
     }
     return true;
 }
 
-
-void dfs(const vector<long long>& primes, vector<long long> v, int sum) {
-    long long start = v[v.size()-1] + 1;
-    for (long long i = start; i < primes.size() && sum + primes[i] < upperBound; i++) {
-        long long n = primes[i];
-        if (isOK(primes, v, n)) {
-            v.push_back(i);
-            if (v.size() >= 5) {
-                int ans = 0;
-                for (long long j = 0; j < v.size(); j++) {
-                    cout << primes[v[j]] << " ";
-                    ans += primes[v[j]];
-                }
-                cout << endl;
-                cout << "A: " << ans << endl;
-                upperBound = min(upperBound, ans);
-            }
-            dfs(primes, v, sum + n);
-        }
+bool canAdd(vector<int>& v, int n) {
+    for (int i = 0; i < v.size(); i++) {
+        if (isPrime(concat(v[i], n)) == false) return false;
+        if (isPrime(concat(n, v[i])) == false) return false;
     }
+    return true;
+}
+
+void printAnswer(vector<int> v) {
+    int ans = 0;
+    for (int i = 0; i < v.size(); i++) {
+        cerr << v[i] << " ";
+        ans += v[i];
+    }
+    cout << endl;
+    cout << "A: " << ans << endl;
+
 }
 
 int main() {
@@ -70,9 +52,31 @@ int main() {
             for (long long j = i*2; j <= MAX_PRIME; j+=i) isPrime[j] = false;
         }
     }
-
-    for (long long i = 0; i < primes.size(); i++) {
-        vector<long long> v(1, i);
-        dfs(primes, v, primes[i]);
+    
+    vector<int> v;
+    for (int i = 0; i < primes.size(); i++) {
+        v.push_back(primes[i]);
+        for (int j = i+1; j < primes.size(); j++) {
+            if (!canAdd(v, primes[j])) continue;
+            v.push_back(primes[j]);
+            for (int k = j+1; k < primes.size(); k++) {
+                if (!canAdd(v, primes[k])) continue;
+                v.push_back(primes[k]);
+                for (int l = k+1; l < primes.size(); l++) {
+                    if (!canAdd(v, primes[l])) continue;
+                    v.push_back(primes[l]);
+                    for (int m = l+1; m < primes.size(); m++) {
+                        if (!canAdd(v, primes[m])) continue;
+                        v.push_back(primes[m]);
+                        printAnswer(v);
+                        v.pop_back();
+                    }
+                    v.pop_back();
+                }
+                v.pop_back();
+            }
+            v.pop_back();
+        }
+        v.pop_back();
     }
 }
